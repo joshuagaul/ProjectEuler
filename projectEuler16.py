45efd1af0c162caafe4df2f1398b207abcc89ce8
Joshua Gaul <joshua@Joshuas-MacBook-Pro.local>
2019-03-18 21:45:43 -0400

    cleared out all of the work from two years ago

--- projectEuler16.py
+++
@@ -1,9 +0,0 @@
-def digitSum(number):
-	numString = str(number)
-	summation = 0
-	for letter in numString:
-		summation = summation + int(letter)
-	return summation
-
-def finalAnswer():
-	return digitSum(2**1000)
